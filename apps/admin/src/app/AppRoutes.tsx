a89b986926f4e49d366cfe89c2ad4c4e7a9ce30e
Logan Martlew <logan.martlew@gmail.com>
2022-08-01 01:01:39 +1200

    Setup react router

---
+++ apps/admin/src/app/AppRoutes.tsx
@@ -0,0 +1,23 @@
+import { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const AppRoutes: FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/projects" element={<h1>View Projects</h1>} />
+        <Route path="/projects/edit" element={<h1>Edit Project</h1>} />
+        <Route path="/projects/:id" element={<h1>View Project Details</h1>} />
+        <Route path="/milestones" element={<h1>View Milestones</h1>} />
+        <Route path="/milestones/edit" element={<h1>Edit Milestone</h1>} />
+        <Route
+          path="/milestones/:id"
+          element={<h1>View Milestone Details</h1>}
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
