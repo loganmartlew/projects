0b8965f230650a2cf6e41deef34e748678b55672
Logan Martlew <logan.martlew@gmail.com>
2022-08-01 17:59:05 +1200

    Prettier

--- apps/admin/src/app/AppRoutes.tsx
+++ apps/admin/src/app/AppRoutes.tsx
@@ -8,13 +8,13 @@ const AppRoutes: FC = () => {
     <Router>
       <Routes>
         <Route element={<NavLayout />}>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/projects" element={<h1>View Projects</h1>} />
-          <Route path="/projects/edit" element={<NewProjectPage />} />
-          <Route path="/projects/:id" element={<h1>View Project Details</h1>} />
-          <Route path="/milestones/edit" element={<h1>Edit Milestone</h1>} />
+          <Route path='/' element={<h1>Home</h1>} />
+          <Route path='/projects' element={<h1>View Projects</h1>} />
+          <Route path='/projects/edit' element={<NewProjectPage />} />
+          <Route path='/projects/:id' element={<h1>View Project Details</h1>} />
+          <Route path='/milestones/edit' element={<h1>Edit Milestone</h1>} />
           <Route
-            path="/milestones/:id"
+            path='/milestones/:id'
             element={<h1>View Milestone Details</h1>}
           />
         </Route>
