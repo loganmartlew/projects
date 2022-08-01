3cfd2db33f086a2147b4f7bc135be565c0d2911c
Logan Martlew <logan.martlew@gmail.com>
2022-08-01 17:57:33 +1200

    Added react hook form to project form

--- apps/admin/src/features/projects/ProjectForm.tsx
+++ apps/admin/src/features/projects/ProjectForm.tsx
@@ -1,20 +1,27 @@
 import { ProjectStatus } from '@project-tracker/types';
 import { FC } from 'react';
+import { useForm } from 'react-hook-form';
 
 const ProjectForm: FC = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" />
+        <label htmlFor='name'>Name</label>
+        <input type='text' id='name' {...register('name')} />
       </div>
       <div>
-        <label htmlFor="description">Description</label>
-        <textarea id="description" />
+        <label htmlFor='description'>Description</label>
+        <textarea id='description' {...register('description')} />
       </div>
       <div>
-        <label htmlFor="status">Status</label>
-        <select id="status">
+        <label htmlFor='status'>Status</label>
+        <select id='status' {...register('status')}>
           {Object.values(ProjectStatus).map((status) => (
             <option key={status} value={status}>
               {status}
@@ -22,7 +29,7 @@ const ProjectForm: FC = () => {
           ))}
         </select>
       </div>
-      <button type="submit">Create Project</button>
+      <button type='submit'>Submit</button>
     </form>
   );
 };
